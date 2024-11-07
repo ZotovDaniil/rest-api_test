9f4baa9b7b6213940aebb62043a433c364d82085
Daniil Zotov <danjke99@yandex.ru>
2024-11-07 11:41:34 +0300

    added attach

--- src/test/java/tests/ReqresTest.java
+++ src/test/java/tests/ReqresTest.java
@@ -1,10 +1,19 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.specification.ResponseSpecification;
 import models.lombok.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
@@ -18,6 +27,31 @@ import static specs.ReqresSpec.*;
 
 
 public class ReqresTest extends TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        com.codeborne.selenide.Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
     @Test
     @DisplayName("Успешное обновление данных")
     void successfulUpdateTest() {
